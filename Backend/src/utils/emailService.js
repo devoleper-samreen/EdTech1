ea3eb6cedad4c4f4b51aeb58ab1073a252a60c51
Samreen <samreenmalik52292@gmail.com>
2026-03-22 18:33:47 +0530

    fix: smtp config for production

--- Backend/src/utils/emailService.js
+++ Backend/src/utils/emailService.js
@@ -1,10 +1,15 @@
 import nodemailer from 'nodemailer';
 
 const getTransporter = () => nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
